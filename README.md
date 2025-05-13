82deaf798d9d52f48d19553775fe9641bdac468a
Tobias Windisch <tobias.windisch@hs-kempten.de>
2025-05-13 08:28:50 +0200

    add link to docs and update complex line heuristics
    Signed-off-by: Tobias Windisch <tobias.windisch@hs-kempten.de>

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 `LineFLow` is a python framework to simulate assembly lines. It allows to model
 arbitrary discrete part assembly lines and provides an `gymnasium` environment to
-optimize them with reinforcement learning.
+optimize them with reinforcement learning. The documentation can be
+found [here](https://hs-kempten.github.io/lineflow/).
 
 ![til](docs/imgs/lineflow.gif)
 # Install
--- lineflow/examples/complex_line.py
+++ lineflow/examples/complex_line.py
@@ -1,5 +1,5 @@
 import numpy as np
-import random
+from gekko import GEKKO
 from lineflow.simulation import (
     Line,
     Sink,
@@ -10,8 +10,34 @@ from lineflow.simulation import (
     WorkerPool,
 )
 
-def index_for_waiting_time(waiting_times, waiting_time):
-    return np.argmin(np.abs(waiting_times - waiting_time))
+
+def compute_balanced_optimum(state, n_assemblies, n_workers):
+    processing_times = np.array([state[f'A{i}']['processing_time'].value for i in range(n_assemblies)])
+
+    m = GEKKO(remote=False)
+
+    n = m.Array(m.Var, dim=n_assemblies, value=n_workers//n_assemblies, ub=n_workers, lb=0, integer=True)
+
+    # Intermediate variables
+    times = []
+    for i in range(n_assemblies):
+        t = m.Intermediate(processing_times[i] * m.exp(-(n[i]) * 0.3) + processing_times[i] * 0.1)
+        times.append(t)
+    t_mean = m.Intermediate(1/n_assemblies * m.sum([t for t in times]))
+
+    # Equations
+    m.Equation(m.sum(n) == n_workers)
+    for i in range(n_assemblies-1):
+        m.Equation(n[i] <= n[i+1])
+
+    # Objective
+    m.Minimize(m.sum([(t - t_mean)**2 for t in times]))
+   
+    m.options.SOLVER = 1
+    m.solve(debug=0, disp=False)
+
+    m.cleanup()
+    return np.array([n[i].value[0] for i in range(n_assemblies)]).astype(int)
 
 
 def get_last_filled_buffer(fills_btw_assemblies):
@@ -31,61 +57,96 @@ def get_fill_factor(fills_assembly_switch, fills_btw_assemblies):
             0.1 * (np.sum(fills_btw_assemblies) / len(fills_btw_assemblies)))
 
 
-class ClAgent():
-    def __init__(self, source_waiting_time, processing_time_diff, complex_line):
-        self.source_waiting_time = source_waiting_time
-        self.processing_time_diff = processing_time_diff
+def index_for_waiting_time(waiting_times, waiting_time):
+    return np.argmin(np.abs(waiting_times - waiting_time))
 
-        self.n_workers = complex_line.n_workers
-        self.n_assemblies = complex_line.n_assemblies
 
-        self.part_count = -1 # set to -1 so that shuffle is called at the beginning
+def make_agent(
+    state,
+    n_assemblies,
+    n_workers,
+    waiting_time=0,
+    ramp_up_waiting_time=0,
+    get_max_reward=False
+):
+    # worker distribution
+    workers = compute_balanced_optimum(state, n_assemblies, n_workers)
 
-    def shuffle_if_needed(self, state, actions):
-        df = state.df()
-        processing_times = [df[f"A{i}_processing_time"].iloc[-1] for i in range(self.n_assemblies)]
-
-        worker_assignments = [worker.value for worker in state.get_actions()["Pool"]]
-        worker_names = [worker.name for worker in state.get_actions()["Pool"]]
-
-        # get the fastest process that has workers assigned to it
-        fastest_stations = np.argsort(processing_times)
-        for i in fastest_stations:
-            if i in worker_assignments:
-                fastest_station = i
-                break
-
-        slowest_station = np.argmax(processing_times)
-
-        if processing_times[slowest_station] - processing_times[fastest_station] > self.processing_time_diff:
-            # assign worker from fastest to slowest station
-            worker_of_fast_station = np.where(worker_assignments == fastest_station)[0][0]
-            worker_assignments[worker_of_fast_station] = slowest_station
-        actions['Pool'] = dict(zip(worker_names, worker_assignments))
-
-    def actions_for_switch(self, actions, state):
-        # Fetch from buffer where fill is largest
-        fills = np.array(
-            [state[f'Buffer_Switch_to_A{i}']['fill'].value for i in range(self.n_assemblies)]
-        )
-        min = np.min(fills)
-        index = np.where(fills == min)[0][-1]  # prioritize last buffer on line
-        actions['Switch'] = {
-            'index_buffer_out': index
-        }
-
-    def __call__(self, state, env):
+    def agent(state, env):
         actions = {}
 
-        self.actions_for_switch(actions, state)
+        # worker assignment
+        worker_assignment = np.concatenate(
+            [np.full(workers[i], i) for i in range(n_assemblies)]
+        )
+        worker_names = [worker.name for worker in state.get_actions()["Pool"]]
+        actions['Pool'] = dict(zip(worker_names, worker_assignment))
 
-        # set waiting time
-        actions['Source'] = {'waiting_time': self.source_waiting_time}
-        # shuffel only after new part is produced
-        if state['EOL']['n_parts_produced'].value > self.part_count:
-            self.part_count = state['EOL']['n_parts_produced'].value
-            self.shuffle_if_needed(state, actions)
+        # get fills of all buffers between the switch and the assemblys
+        fills_assembly_switch = np.array(
+            [state[f'Buffer_Switch_to_A{i}']['fill'].value for i in range(n_assemblies)]
+        )
+        # get fills of all buffers between the assemblys
+        fills_btw_assemblies = np.array(
+            [state[f'Buffer_A{i}_to_A{i+1}']['fill'].value for i in range(n_assemblies-1)]
+        )
+
+        waiting_times = state['Source']['waiting_time'].categories
+
+        if get_max_reward:
+            actions['Switch'] = {'index_buffer_out': fills_assembly_switch.argmin()}
+            actions['Source'] = {'waiting_time': 0}
+
+        else:
+            # find last filled buffer between the assemblys
+            if (fills_assembly_switch[get_last_filled_buffer(fills_btw_assemblies)] == 0
+                and fills_btw_assemblies[get_last_filled_buffer(fills_btw_assemblies) - 1] > 0):
+                actions['Switch'] = {
+                    'index_buffer_out': get_last_filled_buffer(fills_btw_assemblies)
+                }
+                actions['Source'] = {'waiting_time': 0}
+            # check buffer to the first assembly and buffer between first and second assembly
+            elif fills_assembly_switch[0] == 0 and fills_btw_assemblies[0] <= 0.5:
+                actions['Switch'] = {'index_buffer_out': 0}
+                # ramp up
+                if state['EOL']['n_parts_produced'].value == 0:
+                    actions['Source'] = {
+                        'waiting_time': index_for_waiting_time(
+                            waiting_times, ramp_up_waiting_time
+                        )
+                    }
+                else:
+                    actions['Source'] = {'waiting_time': 0}
+            else:
+                # ramp up, distribute greedy, up to last filled buffer between the assemblys
+                if state['EOL']['n_parts_produced'].value == 0:
+                    actions['Switch'] = {
+                        'index_buffer_out': get_filled_buffers(
+                            fills_assembly_switch, fills_btw_assemblies
+                        )
+                    }
+                    actions['Source'] = {
+                        'waiting_time': index_for_waiting_time(
+                            waiting_times, ramp_up_waiting_time
+                        )
+                    }
+                # distribute greedy to all ass., waiting_time depends on fills of buffers
+                else:
+                    actions['Switch'] = {
+                        'index_buffer_out': fills_assembly_switch.argmin()
+                    }
+                    actions['Source'] = {
+                        'waiting_time': np.round(
+                            get_fill_factor(
+                                fills_assembly_switch, fills_btw_assemblies
+                            ) * index_for_waiting_time(
+                                waiting_times, waiting_time
+                                )
+                        ).astype(int)
+                    }
         return actions
+
+    return agent
 
 
 class ComplexLine(Line):
@@ -99,23 +160,16 @@ class ComplexLine(Line):
             n_carriers=20,
             alternate=True,
             assembly_condition=30,
-            random_state=0,
             *args,
-            **kwargs,
+            **kwargs
     ):
         self.n_carriers = n_carriers
         self.alternate = alternate
         self.n_assemblies = n_assemblies
         self.n_workers = n_workers
         self.assembly_condition = assembly_condition
-        self.random_state = random_state
-        self.processing_times = [10+30*i for i in range(self.n_assemblies)]
-        super().__init__(random_state=random_state, *args, **kwargs)
-        self.reset(random_state)
 
-    def reset(self, random_state=None):
-        super().reset(random_state=random_state)
-        random.shuffle(self.processing_times)
+        super().__init__(*args, **kwargs)
 
     def build(self):
         magazine = Magazine(
@@ -131,7 +185,7 @@ class ComplexLine(Line):
 
         sink = Sink(
             'EOL',
-            position=(self.n_assemblies*200-150, 100),
+            position=(self.n_assemblies*100-50, 100),
             processing_time=2
         )
 
@@ -139,9 +193,8 @@ class ComplexLine(Line):
 
         source = Source(
             'Source',
-            position=((self.n_assemblies/2)*200+100, 150),
+            position=((self.n_assemblies/2)*100+200, 150),
             processing_time=1,
-            waiting_time_step=1,
             unlimited_carriers=True,
             carrier_capacity=1,
             actionable_waiting_time=True,
@@ -152,7 +205,7 @@ class ComplexLine(Line):
 
         switch = Switch(
             'Switch',
-            position=((self.n_assemblies/2)*200-50, 150),
+            position=((self.n_assemblies/2)*100, 150),
             alternate=self.alternate,
             processing_time=0,
         )
@@ -161,12 +214,11 @@ class ComplexLine(Line):
 
         # Create assemblies
         assemblies = []
-
         for i in range(self.n_assemblies):
             a = Assembly(
                 f'A{i}',
-                position=((i+1)*200-150, 300),
-                processing_time=self.processing_times[i],
+                position=((i+1)*100-50, 300),
+                processing_time=16+4*i,
                 worker_pool=pool,
                 NOK_part_error_time=2,
             )
@@ -182,14 +234,15 @@ class ComplexLine(Line):
 
 
 if __name__ == '__main__':
-    ramp_up_waiting_time = 6
-    waiting_time = 2
-    n_assemblies = 3
+    ramp_up_waiting_time = 10
+    waiting_time = 5
+    n_assemblies = 5
     n_workers = 3*n_assemblies
     scrap_factor = 1/n_assemblies
 
     line = ComplexLine(
         realtime=False,
+        factor=0.05,
         alternate=False,
         n_assemblies=n_assemblies,
         n_workers=n_workers,
@@ -199,19 +252,16 @@ if __name__ == '__main__':
         assembly_condition=30
     )
 
-    agent = ClAgent(
-        source_waiting_time=4,
-        processing_time_diff=0.9,
-        complex_line=line,
-    )
-
-    line.run(
-        simulation_end=4000,
-        agent=agent,
-        capture_screen=False,
-        show_status=True,
-        visualize=True,
+    agent = make_agent(
+        state=line.state,
+        ramp_up_waiting_time=ramp_up_waiting_time,
+        waiting_time=waiting_time,
+        n_assemblies=n_assemblies,
+        n_workers=n_workers,
+        get_max_reward=False
         )
+
+    line.run(simulation_end=4000, agent=agent, capture_screen=False, show_status=True, visualize=False)
     print("Produced: ", line.get_n_parts_produced())
     print("Scrap: ", line.get_n_scrap_parts())
-    print("Reward: ", line.get_n_parts_produced() - line.get_n_scrap_parts()*scrap_factor)
+    print("Reward: ",  line.get_n_parts_produced() - line.get_n_scrap_parts()*scrap_factor)
